09dcad661cd90a2a7ae239fed3cc010708363684
Кондратович Дарья <darya.kondratovich@gmail.com>
2023-03-29 12:30:14 +0300

    fix dag

--- dag_for_etl_api.py
+++ dag_for_etl_api.py
@@ -1,62 +1,61 @@
 import pendulum
-from airflow import DAG
-from airflow.decorators import task
+from airflow.decorators import dag, task
 from datetime import timedelta
-from airflow.operators.python import PythonOperator
+import json
+
 
 default_args = {
-    'owner': 'darya_kondratovich', 
-    'depend_on__past': False, 
-    'email': ['darya.kondratovich@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 55, 
+    'owner': 'darya_kondratovich',
+    'retries': 10, 
     'retry_delay': timedelta(minutes=5)
 }
-with DAG(
+@dag(
     'ETL_process_new',
     default_args=default_args,
     description='A simple tutorial ETL',
-    schedule='@once',
+    schedule='5 * * * *',
     catchup=False,
     start_date=pendulum.datetime(2023, 3, 1, tz='Europe/Moscow')
+) 
+def etl_process():
 
-) as dag:
-    @task(task_id='extract_data')
-    def extract_data(**kwargs):
+    @task()
+    def extract_data():
         import requests as r
-        import json
 
         url = ('http://universities.hipolabs.com/search?country=Kazakhstan')
         response = r.get(url)
         if response.status_code == 200:
-            return response.json()
+            data = json.loads(response)
+        print(data)
+        return data
 
 
-    @task(task_id='transform_data')  
-    def transform_data(df, **kwargs):
+    @task()  
+    def transform_data(df):
         import pandas as pd
 
         df = pd.DataFrame(extract_data())
         df2 = df.drop(columns = ['state-province'], axis = 1)
         df2['domains'] = df2['domains'].apply(lambda list: list[0])
         df2['web_pages'] = df2['web_pages'].apply(lambda list: list[0])
-        return df2
+        df3 = json.loads(df2)
+        return df3
 
 
-    @task(task_id='load_data')
-    def load_data(df2, **kwargs):
+    @task()
+    def load_data(df3):
         import psycopg2
         from sqlalchemy import create_engine
 
         engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
         engine.connect()
-        df2.head(n=0).to_sql(name = 'education_kz', con=engine, if_exists='replace')
-        return df2.to_sql(name = 'education_kz', con=engine, if_exists='append')
+        df3.head(n=0).to_sql(name = 'education_kz', con=engine, if_exists='replace')
+        return df3.to_sql(name = 'education_kz', con=engine, if_exists='append')
 
     extract = extract_data()
     transform = transform_data(extract)
-    load = load_data(transform)    
+    load_data(transform)    
 
-    extract >> transform >> load
+etl_process()
 
